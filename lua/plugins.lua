330ecd627fdfb946ce98e4df653eb41df24124bc
Subho <contact.subhoghosh@gmail.com>
2023-01-24 23:34:36 +0530

    init

---
+++ lua/plugins.lua
@@ -0,0 +1,14 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    tag = 'nightly'
+  }
+  use 'neovim/nvim-lspconfig'
+end)
