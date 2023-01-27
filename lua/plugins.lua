dc09bcfb4b2d7c9543afc5b68bf081b6496bd27f
Subho <contact.subhoghosh@gmail.com>
2023-01-27 16:59:44 +0530

    add telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,4 +30,9 @@ return require('packer').startup(function(use)
     'jose-elias-alvarez/null-ls.nvim',
     requires = {'nvim-lua/plenary.nvim'}
   }
+
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
 end)
