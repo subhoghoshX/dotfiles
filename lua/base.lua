761a756643a8b817bf807a00c0d77f540600739e
Subho <contact.subhoghosh@gmail.com>
2023-02-01 22:35:20 +0530

    assign <Space> to <Nop>

--- lua/base.lua
+++ lua/base.lua
@@ -9,6 +9,7 @@ vim.opt.shiftwidth = 2 -- Move two space when pressing <Shift>
 vim.opt.laststatus = 3 -- One global statusline
 vim.api.nvim_set_hl(0, 'WinSeparator', { guibg=None }) -- Remove thick border bg b/w splits
 
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = ' '
 
 vim.opt.timeout = false -- Make leader wait
