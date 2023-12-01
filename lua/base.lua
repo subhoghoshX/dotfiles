f4855f6fa4952152305fc902b4a2819fb6a39ee4
Subho <contact.subhoghosh@gmail.com>
2023-12-01 23:09:23 +0530

    add eslint & tailwindcss language server

--- lua/base.lua
+++ lua/base.lua
@@ -1,5 +1,7 @@
 vim.opt.number = true
 vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.opt.signcolumn = 'yes' -- Always keep the space for lsp signs and git signs
 
 vim.opt.expandtab = true -- Convert tab to spaces
 vim.opt.shiftwidth = 2 -- Move 2 spaces when pressing <Tab>
--- lua/plugin-setups/nvim-lspconfig.lua
+++ lua/plugin-setups/nvim-lspconfig.lua
@@ -2,3 +2,5 @@ local lspconfig = require('lspconfig')
 
 lspconfig.tsserver.setup({})
 lspconfig.emmet_language_server.setup({})
+lspconfig.eslint.setup({})
+lspconfig.tailwindcss.setup({})
