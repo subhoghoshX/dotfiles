2425fe29414b9e392c2548f74e1ea69b2274f2c6
Subho <contact.subhoghosh@gmail.com>
2023-12-02 20:46:11 +0530

    improve leader behavior by making it wait and not combine with the next key press

--- lua/base.lua
+++ lua/base.lua
@@ -6,4 +6,6 @@ vim.opt.signcolumn = 'yes' -- Always keep the space for lsp signs and git signs
 vim.opt.expandtab = true -- Convert tab to spaces
 vim.opt.shiftwidth = 2 -- Move 2 spaces when pressing <Tab>
 
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
+vim.opt.timeout = false -- Make leader wait
