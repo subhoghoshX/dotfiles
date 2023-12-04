3fd7dc2364c9ae176df5675ccd8177394bc12987
Subho <contact.subhoghosh@gmail.com>
2023-12-04 16:18:51 +0530

    set some lsp keymaps
    e.g. Show diagnostics in a floating window, jump to definition, hover, rename, references.. and a few more (that I don't understand properly yet)...

--- lua/plugin-setups/nvim-lspconfig.lua
+++ lua/plugin-setups/nvim-lspconfig.lua
@@ -4,3 +4,21 @@ lspconfig.tsserver.setup({})
 lspconfig.emmet_language_server.setup({})
 lspconfig.eslint.setup({})
 lspconfig.tailwindcss.setup({})
+
+-- Set the global shortcuts
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}), -- I don't understand what this does
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+
+    vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, opts) -- ?
+    vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts) -- ?
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts) -- ?
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
+  end
+})
